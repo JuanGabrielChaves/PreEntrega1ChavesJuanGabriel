15b759d81bf44a0919b648082e424bd1833de61e
Juan Gabriel Chaves <achavesjg@gmail.com>
2024-04-23 22:13:42 -0300

    Agregado del Footer

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,12 +4,14 @@ import { ThemeProvider } from "@mui/material";
 import NavBar from "../../components/layout/navBar/NavBar";
 import { Outlet } from "react-router-dom";
 import { themaClaro } from "../common/themeStyles.js";
+import Footer from "./footer/Footer.jsx";
 const Layout = () => {
     return (
         <ThemeProvider theme={themaClaro}>
             <NavBar />
             {/* Al manejar rutas hay que usar Outlet en vez de children */}
             <Outlet />
+            <Footer />
         </ThemeProvider>
     );
 };
---
+++ src/components/layout/footer/Footer.jsx
@@ -0,0 +1,38 @@
+/** @format */
+
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+const Footer = () => {
+    return (
+        <div>
+            {" "}
+            <Box
+                sx={{
+                    width: "100%",
+                    height: "auto",
+                    paddingTop: "1rem",
+                    paddingBottom: "1rem",
+                }}>
+                <Container maxWidth="lg">
+                    <Grid container direction="column" alignItems="center">
+                        <Grid item xs={12}>
+                            <Link to={"/"} className="logoFooter">
+                                <Typography color="black" variant="h5">
+                                    The BookStore
+                                </Typography>
+                            </Link>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography color="textSecondary" variant="subtitle1">
+                                {`${new Date().getFullYear()} | React | CoderHouse | Juan Gabriel Chaves`}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </div>
+    );
+};
+
+export default Footer;
